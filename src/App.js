513bf871b423e2041e1626be437485b2068b751d
Hoàng Đại <daihoangnam3it@gmail.com>
2020-09-21 09:32:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
